547342844f903e68dbd026ec0998183f671e8908
maria alvarez <mar.alvareg@gmail.com>
2020-11-12 10:54:08 -0300

    erd

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :blogs
-  has_many :appointments, through: :appointments_users
+  has_and_belongs_to_many :appointments, through: :appointments_users
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and 
   devise :database_authenticatable, :registerable,
